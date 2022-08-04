fb694158fb7aa129edf5b9a62372ca68de7dd14b
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-08-04 13:56:02 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 /** @format */
 
-console.log("[+] Starting The Script Mate");
-import Log from "./middlewares/Log";
-import Locals from "./providers/locals";
 import App from "./providers/App";
 
 App.ClearConsole();
